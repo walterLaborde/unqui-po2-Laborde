102ab6049f68127a4ee2b73876649b8c53caaf20
Walter <walter.laborde@hotmail.com>
2022-04-21 13:02:27 -0300

    modificaciones: lo volví a crear porque no me anda

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,50 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Counter {
+	
+	private List<Integer> numeros = new ArrayList<Integer>();
+	
+	
+	public List<Integer> getNumeros() {
+		return numeros;
+	}
+
+	public void setNumeros(List<Integer> numeros) {
+		this.numeros = numeros;
+	}
+
+	public int contarPares() {
+		int cantidad = 0;
+		for (Integer numero : this.getNumeros()) {
+			if (numero % 2 == 0) cantidad++;
+		}
+		return cantidad;
+	}
+	
+	public int contarImpares() {
+		int cantidad = 0;
+		for (Integer numero : this.getNumeros()) {
+			if (numero % 2 != 0) cantidad++;
+		}
+		return cantidad;
+	}
+	
+	public int cantMultiplosDe(int num) {
+		int cantidad = 0;
+		for (Integer numero : this.getNumeros()) {
+			if (numero % num == 0) cantidad++;
+		}
+		return cantidad;
+	}
+
+
+	public void addNumber(int i) {
+		// TODO Auto-generated method stub
+		this.getNumeros().add(i);
+	}
+	
+}
---
+++ test/ar/edu/unq/po2/tp3/CounterTestCase.java
@@ -0,0 +1,46 @@
+package ar.edu.unq.po2.tp3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CounterTestCase {
+	
+	private Counter counter;
+	/**
+	* Crea un escenario de test básico, que consiste en un contador
+	* con 10 enteros
+	*
+	* @throws Exception
+	*/
+	@BeforeEach
+	public void setUp() throws Exception {
+	//Se crea el contador
+	counter = new Counter();
+	//Se agregan los numeros. Un solo par y nueve impares
+	counter.addNumber(1);
+	counter.addNumber(3);
+	counter.addNumber(5);
+	counter.addNumber(7);
+	counter.addNumber(9);
+	counter.addNumber(1);
+	counter.addNumber(1);
+	counter.addNumber(1);
+	counter.addNumber(1);
+	counter.addNumber(4);
+	}
+	/**
+	 * Verifica la cantidad de pares
+	 */
+	
+	@Test
+	public void testEvenNumbers() {
+		// Getting the even occurrences
+		int amount = counter.contarPares();
+		// I check the amount is the expected one
+		assertEquals(amount, 9);
+		}
+	}
+
+}
